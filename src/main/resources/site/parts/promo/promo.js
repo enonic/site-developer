7b1cb65e85f4769af76eabedc800968d1a9e20e4
ashklianko <asl@enonic.com>
2017-11-14 14:26:41 +0300

    promotion part - version 2 #23

--- src/main/resources/site/parts/promo/promo.js
+++ src/main/resources/site/parts/promo/promo.js
@@ -1,14 +1,20 @@
-var thymeleafLib = require('/lib/xp/thymeleaf');
+var libs = {
+    thymeleaf: require('/lib/xp/thymeleaf'),
+    util: require('/lib/util')
+}
+
 
 exports.get = function (req) {
 
     var model = {
+        linkDocs: libs.util.getSiteUrl() + 'docs',
+        linkGuides: libs.util.getSiteUrl() + 'guides'
     };
 
     var view = resolve('promo.html');
 
     return {
-        body: thymeleafLib.render(view, model),
+        body: libs.thymeleaf.render(view, model),
         contentType: 'text/html; charset=UTF-8'
     };
 };
