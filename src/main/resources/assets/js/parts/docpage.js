60d488f4b5efc6e97acd835942201dfe3e190119
ashklianko <asl@enonic.com>
2017-12-28 00:55:06 +0100

    Add "Doc Header" #55
    -sticky header

--- src/main/resources/assets/js/parts/docpage.js
+++ src/main/resources/assets/js/parts/docpage.js
@@ -5,4 +5,39 @@ $(function () {
         window.location.href = url;
     });
 
+    $('.docpage__header').each(function (i) {
+        var docHeader = this;
+        var docHeaderMargin = parseInt(window.getComputedStyle(docHeader).getPropertyValue('margin-bottom'));
+        var docHeaderHeight = docHeader.getBoundingClientRect().height + docHeaderMargin;
+        var sticky = docHeader.offsetTop;
+        var docContent = docHeader.nextElementSibling;
+        var toc = docContent.querySelector('.toc');
+
+        window.addEventListener('scroll', updateHeader);
+        window.addEventListener('availablesizechange', updateHeader);
+        window.addEventListener('resize', updateHeader);
+
+        function updateHeader() {
+            if (window.pageYOffset >= sticky) {
+                makeHeaderSticky()
+            } else {
+                makeHeaderStatic();
+            }
+        }
+
+        function makeHeaderSticky() {
+            docHeader.classList.add('sticky');
+            docContent.style.paddingTop = docHeaderHeight + 'px';
+            docHeader.style.width = docContent.getBoundingClientRect().width + 'px';
+            toc.style.top = docHeaderHeight + 'px';
+        }
+
+        function makeHeaderStatic() {
+            docHeader.classList.remove('sticky');
+            docContent.style.paddingTop = '';
+            docHeader.style.width = '';
+            toc.style.top = '';
+        }
+    });
+
 });
