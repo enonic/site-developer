c0311ca3db9f376bc8e1617d76a5408a99343a42
ashklianko <asl@enonic.com>
2018-01-05 18:40:19 +0300

    Special handling of "doc" content in admin #58

--- src/main/resources/site/controllers/doc-controller.js
+++ src/main/resources/site/controllers/doc-controller.js
@@ -1,6 +1,7 @@
 var libs = {
     portal: require('/lib/xp/portal'),
-    content: require('/lib/xp/content')
+    content: require('/lib/xp/content'),
+    thymeleaf: require('/lib/xp/thymeleaf'),
 };
 
 exports.get = handleGet;
@@ -8,20 +9,60 @@ exports.get = handleGet;
 function handleGet(req) {
     var doc = libs.portal.getContent();
 
-    var latestDocVersionId = doc.data.latest;
+    var isPreview = isPreviewMode(req);
 
-    if (!!latestDocVersionId) {
-        var docVersionUrl = libs.portal.pageUrl({
-            id: latestDocVersionId,
-        });
+    if (isPreview) {
+        var view = resolve('/site/pages/available-versions/available-versions.html');
+        var model = {
+            versions: getVersions(doc)
+        };
 
         return {
-            redirect: docVersionUrl
-        };
+            body: libs.thymeleaf.render(view, model),
+            contentType: 'text/html; charset=UTF-8'
+        }
     }
+    else {
+        var latestDocVersionId = doc.data.latest;
 
-    return {
-        body: '<div style="font-size: 21px;color: lightgray;top: 50%;text-align: center;width: 100%;position: absolute;margin-top: -20px;">No docs available</div>',
-        contentType: 'text/html; charset=UTF-8'
+        if (!!latestDocVersionId) {
+            var docVersionUrl = libs.portal.pageUrl({
+                id: latestDocVersionId,
+            });
+
+            return {
+                redirect: docVersionUrl
+            };
+        }
+
+        return {
+            body: '<div style="font-size: 21px;color: lightgray;top: 50%;text-align: center;width: 100%;position: absolute;margin-top: -20px;">No docs available</div>',
+            contentType: 'text/html; charset=UTF-8'
+        }
     }
+}
+
+function isPreviewMode(req) {
+    return req.path.indexOf('preview') != 0;
+}
+
+function getVersions(doc) {
+    var expr = "type = '" + app.name + ":docversion' AND _path LIKE '/content" + doc._path + "/*' ";
+
+    var result = libs.content.query({
+        query: expr,
+        start: 0,
+        count: 100
+    });
+
+    var versions = [];
+    result.hits.forEach(function (version) {
+        versions.push({
+            label: version.displayName,
+            isLatest: version._id == doc.data.latest,
+            url: libs.portal.pageUrl({path: version._path})
+        })
+    });
+
+    return versions;
 }
