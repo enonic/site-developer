fe510ba400bba0fe647d6765719e26e1ad19e539
ashklianko <asl@enonic.com>
2018-01-18 17:12:52 +0300

    Final page/header options and tuning #87

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -1,3 +1,4 @@
+// Imports
 var libs = {
     portal : require('/lib/xp/portal'),
     thymeleaf : require('/lib/xp/thymeleaf'),
@@ -5,38 +6,44 @@ var libs = {
     util: require('/lib/util')
 };
 
+// Imported functions
+var getSite = libs.portal.getSite;
+var getContent = libs.portal.getContent;
+var serviceUrl = libs.portal.serviceUrl;
+var render = libs.thymeleaf.render;
+var getMenuTree = libs.menu.getMenuTree;
+var getSiteUrl = libs.util.getSiteUrl;
+
 // Handle GET request
 exports.get = handleGet;
 exports.post = handleGet;
 
 function handleGet(req) {
 
-    var site = libs.portal.getSite(); // Current site
-    var content = libs.portal.getContent(); // Current content
+    var site = getSite(); // Current site
+    var content = getContent(); // Current content
     var view = resolve('default.html'); // The view to render
     var model = createModel(); // The model to send to the view
 
     function createModel() {
         var model = {};
 
+        // Used directly in view
         model.mainRegion = content.page.regions['main'];
+        model.pageColor = getPageColor();
+        model.bodyClass = getBodyClass();
+
+        // Used in fragments (not mentioned in view)
         model.sitePath = site['_path'];
         model.currentPath = content._path;
         model.pageTitle = getPageTitle();
-        model.menuItems = libs.menu.getMenuTree(3);
-
-        model.searchResultPageUrl = libs.util.getSiteUrl() + 'search';
-        model.serviceUrl = libs.portal.serviceUrl({
+        model.menuItems = getMenuTree(3);
+        model.searchResultPageUrl = getSiteUrl() + 'search';
+        model.serviceUrl = serviceUrl({
             service: 'search'
         });
-
-        // Defines whether page header is layered or not
-        model.headerClass = getHeaderClass();
-        model.bodyClass = getBodyClass();
-
-        // Header logo and menu button color
-        model.headerColor = content.page.config['headerColor'] === 'white' ? 'dark' : null;
-
+        model.headerClass = getHeaderClass(); // Defines whether page header is layered or not
+        model.headerColor = getHeaderColor(); // Header logo and menu button color
         model.showHeaderSearch = true;
 
         return model;
@@ -74,8 +81,16 @@ function handleGet(req) {
         return content.page.config['headerType'];
     }
 
+    function getHeaderColor() {
+        return content.page.config['headerColor'];
+    }
+
+    function getPageColor() {
+        return content.page.config['pageColor'];
+    }
+
     return {
-        body: libs.thymeleaf.render(view, model)
+        body: render(view, model)
     }
 
 }
