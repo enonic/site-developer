ec862c53bacf77564fa377cd1a2f9228d4999fd4
Christian Westgaard <cwe@enonic.com>
2018-01-19 13:52:33 +0100

    First controller converted to ECMAScript 2015

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+    extends: 'airbnb-base',
+
+    globals: {
+      app: false,
+      resolve: false,
+      __: false
+    },
+    rules: { // https://eslint.org/docs/rules
+        'comma-dangle': ['error', {
+            arrays: 'ignore',
+            objects: 'never',
+            imports: 'never',
+            exports: 'never',
+            functions: 'ignore'
+        }],
+        'import/extensions': ['off'],
+        'import/prefer-default-export': ['off'],
+        'import/no-absolute-path': ['off'],
+        'import/no-extraneous-dependencies': ['off'],
+        'import/no-unresolved': ['off'],
+        indent: ['error', 4],
+        'max-len': ['error', 100, 2, {
+            ignoreUrls: true,
+            ignoreComments: true,
+            ignoreRegExpLiterals: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+        }],
+        'object-curly-spacing': ['off'],
+        'spaced-comment': ['off']
+    } // rules
+}; // exports
