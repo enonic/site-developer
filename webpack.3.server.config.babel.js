a3a7453be0bc1ad31156953e2312ccdc24b198be
Christian Westgaard <cwe@enonic.com>
2018-02-07 10:22:16 +0100

    Enonic XP dev mode watches js files in src/main/resources so those cant be ES2015. Thus use es extension

--- webpack.3.server.config.babel.js
+++ webpack.3.server.config.babel.js
@@ -72,7 +72,7 @@ const WEBPACK_CONFIG = {
     }, // output
     resolve: {
         alias: {
-            '/content-types': path.resolve(__dirname, SRC_DIR, 'site', 'content-types', 'index.js'),
+            '/content-types': path.resolve(__dirname, SRC_DIR, 'site', 'content-types', 'index.es'),
             '/lib': path.resolve(__dirname, SRC_DIR, 'lib')
         },
         extensions: ['.es', '.js', '.json']
