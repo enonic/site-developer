ed797302a7dd0b7a622414a5dcf01a046f116b47
Christian Andreas Westgaard <Christian.Westgaard@gjensidige.no>
2018-08-16 10:24:29 +0200

    Fix webpack after upgrading modules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,6 +3,7 @@
 // Imports
 //──────────────────────────────────────────────────────────────────────────────
 import ExtractTextPlugin, {extract as extractText} from 'extract-text-webpack-plugin';
+//import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import glob from 'glob';
 import globImporter from 'node-sass-glob-importer';
 //import MinifyPlugin from 'babel-minify-webpack-plugin';
@@ -33,15 +34,16 @@ const DST_DIR = 'build/resources/main';
 //──────────────────────────────────────────────────────────────────────────────
 const context = path.resolve(__dirname, SRC_DIR);
 const extensions = ['.es', '.js', '.json']; // used in resolve
+const mode = 'production';
 const outputPath = path.join(__dirname, DST_DIR);
 const stats = {
-    colors: true,
-    hash: false,
-    maxModules: 0,
-    modules: false,
-    moduleTrace: false,
-    timings: false,
-    version: false
+	colors: true,
+	hash: false,
+	maxModules: 0,
+	modules: false,
+	moduleTrace: false,
+	timings: false,
+	version: false
 };
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -54,54 +56,55 @@ const ALL_JS_ASSETS_FILES = glob.sync(ALL_JS_ASSETS_GLOB);
 //console.log(`ALL_JS_ASSETS_FILES:${toStr(ALL_JS_ASSETS_FILES)}`);
 
 const SERVER_JS_FILES = glob.sync(`${SRC_DIR}/**/${JS_EXTENSION_GLOB_BRACE}`, {
-    ignore: ALL_JS_ASSETS_FILES
+	ignore: ALL_JS_ASSETS_FILES
 });
 // console.log(`SERVER_JS_FILES:${toStr(SERVER_JS_FILES)}`);
 
 const SERVER_JS_CONFIG = {
-    context,
-    entry: dict(SERVER_JS_FILES.map(k => [
-        k.replace(`${SRC_DIR}/`, '').replace(/\.[^.]*$/, ''), // name
-        `.${k.replace(`${SRC_DIR}`, '')}` // source relative to context
-    ])),
-    externals: [
-        /\/lib\/(enonic|xp)/
-    ],
-    devtool: false, // Don't waste time generating sourceMaps
-    module: {
-        rules: [{
-            test: /\.(es6?|js)$/, // Will need js for node module depenencies
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    babelrc: false, // The .babelrc file should only be used to transpile config files.
-                    comments: false,
-                    compact: false,
-                    minified: false,
-                    plugins: [
-                        'array-includes',
-                        'optimize-starts-with',
-                        'transform-object-assign',
-                        'transform-object-rest-spread'
-                    ],
-                    presets: ['es2015']
-                } // options
-            }] // use
-        }] // rules
-    }, // module
-    output: {
-        path: outputPath,
-        filename: '[name].js',
-        libraryTarget: 'commonjs'
-    }, // output
-    resolve: {
-        alias: {
-            '/content-types': path.resolve(__dirname, SRC_DIR, 'site', 'content-types', 'index.es'),
-            '/lib': path.resolve(__dirname, SRC_DIR, 'lib')
-        },
-        extensions
-    }, // resolve
-    stats
+	context,
+	entry: dict(SERVER_JS_FILES.map(k => [
+		k.replace(`${SRC_DIR}/`, '').replace(/\.[^.]*$/, ''), // name
+		`.${k.replace(`${SRC_DIR}`, '')}` // source relative to context
+	])),
+	externals: [
+		/\/lib\/(enonic|xp)/
+	],
+	devtool: false, // Don't waste time generating sourceMaps
+	mode,
+	module: {
+		rules: [{
+			test: /\.(es6?|js)$/, // Will need js for node module depenencies
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					babelrc: false, // The .babelrc file should only be used to transpile config files.
+					comments: false,
+					compact: false,
+					minified: false,
+					plugins: [
+						'array-includes',
+						'optimize-starts-with',
+						'transform-object-assign',
+						'transform-object-rest-spread'
+					],
+					presets: ['es2015']
+				} // options
+			}] // use
+		}] // rules
+	}, // module
+	output: {
+		path: outputPath,
+		filename: '[name].js',
+		libraryTarget: 'commonjs'
+	}, // output
+	resolve: {
+		alias: {
+			'/content-types': path.resolve(__dirname, SRC_DIR, 'site', 'content-types', 'index.es'),
+			'/lib': path.resolve(__dirname, SRC_DIR, 'lib')
+		},
+		extensions
+	}, // resolve
+	stats
 };
 //console.log(`SERVER_JS_CONFIG:${JSON.stringify(SERVER_JS_CONFIG, null, 4)}`);
 
@@ -110,67 +113,65 @@ const SERVER_JS_CONFIG = {
 // Javascript Assets
 //──────────────────────────────────────────────────────────────────────────────
 const ASSETS_JS_CONFIG = { // Javascript assets
-    context,
-    entry: {
-        'assets/js/main.min': './assets/js/main.es'
-    },
-    devtool: 'source-map', // https://webpack.js.org/configuration/devtool/
-    module: {
-        rules: [{
-            test: /\.(es6?|js)$/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    babelrc: false, // The .babelrc file should only be used to transpile *.babel.js files.
-                    comments: false,
-                    compact: true,
-                    minified: true,
-                    plugins: [
-                        'array-includes',
-                        'optimize-starts-with',
-                        'transform-object-assign',
-                        'transform-object-rest-spread'
-                    ],
-                    presets: [
-                        [
-                            'env', {
-                                modules: false // NOTE svg4everybody fails runtime without this!
-                            }
-                        ]
-                    ]
-                } // options
-            }] // use
-        }] // rules
-    }, // module
-    output: {
-        path: outputPath,
-        filename: '[name].js'
-    },
-    plugins: [
-        new ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }),
-        new UglifyJsPlugin({ // TODO this seems to remove the source map file
-            minimize: true,
-            compress: {
-                warnings: false
-            },
-            parallel: true, // highly recommended
-            sourceMap: true // default is false and overrides devtool? So both is needed.
-        })
-        /*new MinifyPlugin({ // minifyOpts
-        }, { // pluginOpts
-        })*/
-    ], // plugins
-    resolve: {
-        alias: {
-            jquery: 'jquery/src/jquery'
-        },
-        extensions
-    },
-    stats
+	context,
+	entry: {
+		'assets/js/main.min': './assets/js/main.es'
+	},
+	devtool: 'source-map', // https://webpack.js.org/configuration/devtool/
+	mode,
+	module: {
+		rules: [{
+			test: /\.(es6?|js)$/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					babelrc: false, // The .babelrc file should only be used to transpile *.babel.js files.
+					comments: false,
+					compact: true,
+					minified: true,
+					plugins: [
+						'array-includes',
+						'optimize-starts-with',
+						'transform-object-assign',
+						'transform-object-rest-spread'
+					],
+					presets: [
+						[
+							'env', {
+								modules: false // NOTE svg4everybody fails runtime without this!
+							}
+						]
+					]
+				} // options
+			}] // use
+		}] // rules
+	}, // module
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({ // TODO this seems to remove the source map file
+				parallel: true, // highly recommended
+				sourceMap: true // default is false and overrides devtool? So both is needed.
+			})
+		]
+	},
+	output: {
+		path: outputPath,
+		filename: '[name].js'
+	},
+	plugins: [
+		new ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery'
+		})
+	], // plugins
+	resolve: {
+		alias: {
+			jquery: 'jquery/src/jquery'
+		},
+		extensions
+	},
+	stats
 };
 //console.log(`ASSETS_JS_CONFIG:${JSON.stringify(ASSETS_JS_CONFIG, null, 4)}`);
 
@@ -179,70 +180,71 @@ const ASSETS_JS_CONFIG = { // Javascript assets
 // Style assets
 //──────────────────────────────────────────────────────────────────────────────
 const STYLE_ASSETS_CONFIG = { // eslint-disable-line no-unused-vars
-    context,
-    entry: {
-        'assets/css/coderay': './assets/css/coderay.css',
-        'assets/css/critical': './assets/css/critical.scss',
-        'assets/css/non-critical': './assets/css/non-critical.scss'
-    },
-    module: {
-        rules: [{
-            test: /\.s?css$/,
-            use: extractText([
-                /*{
-                    loader: 'style-loader' // creates style nodes from JS strings
-                },*/ {
-                    loader: 'css-loader' // translates CSS into CommonJS
-                }, {
-                    loader: 'sass-loader', // compiles Sass to CSS
-                    options: {
-                        //errLogToConsole: true,
-                        importer: globImporter()//,
-                        //outputStyle: 'compressed'
-                    }
-                }
-            ])
-        }, {
-            test: /\.svg$/,
-            use: {
-                loader: 'svg-url-loader',
-                options: {
-                    // If given will tell the loader not to encode the source
-                    // file if its content is greater than this limit. Defaults
-                    // to no limit. If the file is greater than the limit the
-                    // file-loader is used and all query parameters are passed
-                    // to it.
-                    limit: 100000
-                }
-            }
-        }, {
-            test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-            use: {
-                loader: 'url-loader', //?limit=100000&name=assets/css/[name].[ext]'
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                }
-            }
-        }] // rules
-    }, // module
-    output: {
-        path: outputPath,
-        filename: '[name].css'
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'assets/css/styles.min.css'
-        })
-    ],
-    /*resolve: {
-        modules: [
-            path.resolve(__dirname, SRC_DIR, 'assets'),
-            path.resolve(__dirname, SRC_DIR, 'site', 'assets'),
-            'node_modules'
-        ]
-    },*/
-    stats
+	context,
+	entry: {
+		'assets/css/coderay': './assets/css/coderay.css',
+		'assets/css/critical': './assets/css/critical.scss',
+		'assets/css/non-critical': './assets/css/non-critical.scss'
+	},
+	mode,
+	module: {
+		rules: [{
+			test: /\.s?css$/,
+			use: extractText([
+				/*{
+					loader: 'style-loader' // creates style nodes from JS strings
+				},*/ {
+					loader: 'css-loader' // translates CSS into CommonJS
+				}, {
+					loader: 'sass-loader', // compiles Sass to CSS
+					options: {
+						//errLogToConsole: true,
+						importer: globImporter()//,
+						//outputStyle: 'compressed'
+					}
+				}
+			])
+		}, {
+			test: /\.svg$/,
+			use: {
+				loader: 'svg-url-loader',
+				options: {
+					// If given will tell the loader not to encode the source
+					// file if its content is greater than this limit. Defaults
+					// to no limit. If the file is greater than the limit the
+					// file-loader is used and all query parameters are passed
+					// to it.
+					limit: 100000
+				}
+			}
+		}, {
+			test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+			use: {
+				loader: 'url-loader', //?limit=100000&name=assets/css/[name].[ext]'
+				options: {
+					limit: 100000,
+					name: '[name].[ext]'
+				}
+			}
+		}] // rules
+	}, // module
+	output: {
+		path: outputPath,
+		filename: '[name].css'
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: 'assets/css/styles.min.css'
+		})
+	],
+	/*resolve: {
+		modules: [
+			path.resolve(__dirname, SRC_DIR, 'assets'),
+			path.resolve(__dirname, SRC_DIR, 'site', 'assets'),
+			'node_modules'
+		]
+	},*/
+	stats
 };
 //console.log(`STYLE_ASSETS_CONFIG:${JSON.stringify(STYLE_ASSETS_CONFIG, null, 4)}`);
 
@@ -251,9 +253,9 @@ const STYLE_ASSETS_CONFIG = { // eslint-disable-line no-unused-vars
 // Exports
 //──────────────────────────────────────────────────────────────────────────────
 const WEBPACK_CONFIG = [
-    SERVER_JS_CONFIG,
-    ASSETS_JS_CONFIG/*,
-    STYLE_ASSETS_CONFIG*/
+	SERVER_JS_CONFIG,
+	ASSETS_JS_CONFIG/*,
+	STYLE_ASSETS_CONFIG*/
 ];
 
 //console.log(`WEBPACK_CONFIG:${JSON.stringify(WEBPACK_CONFIG, null, 4)}`);
